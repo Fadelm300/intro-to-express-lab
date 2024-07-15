3e73968f33c7b899435e91947ad5082acbff1f86
Fadem300 <fadel.m200@gmail.com>
2024-07-15 14:21:12 +0300

    lab

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const express = require('express')
 const app = express()
 
 // Define routes here (we'll add them soon)
-//مهمممممم جدا
-// Listen for requests on port 3000
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+// Listen for requests on port 3001
+app.listen(3001, () => {
+    console.log('Listening on port 3001')
   })
 
 
-  
+
 // Define the data array
   const collectibles = [
     { name: 'shiny ball', price: 5.95 },
@@ -47,5 +46,51 @@ app.get('/roll/:number',(req,res)=>{
 })
 
 //-->3
+// Route
+app.get('/collectibles/:index', (req, res) => {
+  const index = parseInt(req.params.index, 10);
+
+  if (isNaN(index) || index < 0 || index >= collectibles.length) {
+    res.send('This item is not yet in stock. Check back soon!');
+  } else {
+    const item = collectibles[index];
+    res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`);
+  }
+});
+
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+//-->4
+// Define the /shoes route
+// 4. Filter Shoes by Query Parameters
+app.get('/shoes', (req, res) => {
+  let filteredShoes = shoes;
 
   
+  const minPrice = parseFloat(req.query.minPrice);
+  const maxPrice = parseFloat(req.query.maxPrice);
+  const type = req.query.type;
+
+  if (!!req.query.minPrice) {
+      filteredShoes = filteredShoes.filter(shoe => shoe.price > minPrice);
+  }
+
+  if (!!req.query.maxPrice) {
+      filteredShoes = filteredShoes.filter(shoe => shoe.price < maxPrice);
+  }
+
+  if (!!req.query.type) {
+      filteredShoes = filteredShoes.filter(shoe => shoe.type.toLowerCase() === type.toLowerCase());
+  }
+
+  res.json(filteredShoes);
+});
